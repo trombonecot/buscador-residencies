e3ff2aecaf62f774d82024138076ba289729e527
Eduard Cot Ros <ecot@iMac-de-Eduard.local>
2016-08-03 22:01:31 +0200

    primer entoutament

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
 
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import {ResidenciaApi} from './api/residencia.api';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>hahahah</h1><router-outlet></router-outlet>',
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+        ResidenciaApi
+    ],
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'ECOT ets el millor';
+ }
